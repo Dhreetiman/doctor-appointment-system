43ef6692347fb483d5f5d76d36fe34b73fcc72e8
dhreetiman1 <dhreetiman@kavarainfratech.com>
2023-06-09 15:26:15 +0530

    new job sccheduled

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 let connectDB = require("./database"); 
 let Gallery = require("./models/Gallery"); 
 let Doctor = require("./models/Doctor"); 
-var CronJob = require('cron').CronJob
+// var CronJob = require('cron').CronJob
+var cron = require('node-cron');
 const cors = require('cors')
 const app = express();
 app.use(cors())
@@ -22,13 +23,33 @@ let addData = async () => {
   await Gallery.insertMany(galleryData)
 }
 
-const cronJob = new CronJob('0 47 13 * * *', function () {
-  console.log('Cron job started');
-  addData()
-  console.log("Data Added");
+// const cronJob = new CronJob('0 10 14 * * *', function () {
+//   console.log('Cron job started');
+//   addData()
+//   console.log("Data Added");
 
-}, "Asia/Kolkata");
-cronJob.start()
+// }, "America/Los_Angeles");
+// cronJob.start()
+
+
+
+ 
+
+ cron.schedule('0 33 15 * * *', () => {
+   console.log('Cron Job');
+   addData()
+   console.log("Data Added");
+ }, {
+   scheduled: true,
+   timezone: "Asia/Kolkata"
+ });
+
+
+
+
+
+
+
 
 app.use('/api/doctor', require('./routes/doctorRoute'))
 app.use('/api/patient', require('./routes/patientRoute'))
